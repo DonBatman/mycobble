721e1c9e35e2dfe0e505dde96a2bac37d52a5339
DonBatman <serfdon@gmail.com>
2025-11-24 11:11:15 -0800

    Code Update

--- init.lua
+++ init.lua
@@ -1,92 +1,92 @@
 --Stone
-minetest.register_craft({
+core.register_craft({
 	type = "shapeless",
 	output = "default:cobble",
 	recipe ={"default:stone"},
 })
-minetest.register_craft({
+core.register_craft({
 	type = "shapeless",
 	output = "default:gravel",
 	recipe ={"default:cobble"},
 })
 
-minetest.register_craft({
+core.register_craft({
 	type = "shapeless",
 	output = "default:sand",
 	recipe ={"default:gravel"},
 })
 
 --Desert Stone
-minetest.register_craft({
+core.register_craft({
 	type = "shapeless",
 	output = "default:desert_cobble",
 	recipe ={"default:desert_stone"},
 })
 
-minetest.register_craft({
+core.register_craft({
 	type = "shapeless",
 	output = "mycobble:desert_gravel",
 	recipe ={"default:desert_cobble"},
 })
 
-minetest.register_craft({
+core.register_craft({
 	type = "shapeless",
 	output = "default:desert_sand",
 	recipe ={"mycobble:desert_gravel"},
 })
 
 --Sandstone
-minetest.register_craft({
+core.register_craft({
 	type = "shapeless",
 	output = "mycobble:sandstone_cobble",
 	recipe ={"default:sandstone","default:sandstone"},
 })
 
-minetest.register_craft({
+core.register_craft({
 	type = "shapeless",
 	output = "mycobble:sandstone_gravel",
 	recipe ={"mycobble:sandstone_cobble"},
 })
 
-minetest.register_craft({
+core.register_craft({
 	type = "shapeless",
 	output = "mycobble:sandstone_sand",
 	recipe ={"mycobble:sandstone_gravel"},
 })
 
 --Silver Stone
-minetest.register_craft({
+core.register_craft({
 	type = "shapeless",
 	output = "mycobble:silver_cobble",
 	recipe ={"default:silver_sandstone","default:silver_sandstone"},
 })
 
-minetest.register_craft({
+core.register_craft({
 	type = "shapeless",
 	output = "mycobble:silver_gravel",
 	recipe ={"mycobble:silver_cobble"},
 })
 
-minetest.register_craft({
+core.register_craft({
 	type = "shapeless",
 	output = "default:silver_sand",
 	recipe ={"mycobble:silver_gravel"},
 })
 
 --Desert Sandstone
-minetest.register_craft({
+core.register_craft({
 	type = "shapeless",
 	output = "mycobble:desert_sandstone_cobble",
 	recipe ={"default:desert_sandstone","default:desert_sandstone"},
 })
 
-minetest.register_craft({
+core.register_craft({
 	type = "shapeless",
 	output = "mycobble:desert_sandstone_gravel",
 	recipe ={"mycobble:desert_sandstone_cobble"},
 })
 
-minetest.register_craft({
+core.register_craft({
 	type = "shapeless",
 	output = "mycobble:desert_sandstone_sand",
 	recipe ={"mycobble:desert_sandstone_gravel"},
@@ -95,7 +95,7 @@ minetest.register_craft({
 ---------------------------------------------------------------------
 --Nodes
 
-minetest.register_node ("mycobble:desert_gravel",{
+core.register_node ("mycobble:desert_gravel",{
 	description = "Desert Gravel",
 	tiles = {"default_gravel.png^[colorize:#8A2908:120"},
 	is_ground_content = true,
@@ -106,7 +106,7 @@ minetest.register_node ("mycobble:desert_gravel",{
 	}),
 })
 
-minetest.register_node ("mycobble:sandstone_gravel",{
+core.register_node ("mycobble:sandstone_gravel",{
 	description = "Sandstone Gravel",
 	tiles = {"default_gravel.png^[colorize:#e3e3ae:120"},
 	is_ground_content = true,
@@ -117,7 +117,7 @@ minetest.register_node ("mycobble:sandstone_gravel",{
 	}),
 })
 
-minetest.register_node ("mycobble:sandstone_sand",{
+core.register_node ("mycobble:sandstone_sand",{
 	description = "Sandstone Sand",
 	tiles = {"default_sand.png^[colorize:#e3e3ae:120"},
 	is_ground_content = true,
@@ -128,7 +128,7 @@ minetest.register_node ("mycobble:sandstone_sand",{
 	}),
 })
 
-minetest.register_node ("mycobble:silver_gravel",{
+core.register_node ("mycobble:silver_gravel",{
 	description = "Silver Gravel",
 	tiles = {"default_gravel.png^[colorize:#c8c8c1:120"},
 	is_ground_content = true,
@@ -139,7 +139,7 @@ minetest.register_node ("mycobble:silver_gravel",{
 	}),
 })
 
-minetest.register_node ("mycobble:silver_cobble",{
+core.register_node ("mycobble:silver_cobble",{
 	description = "Silver Cobble",
 	tiles = {"default_cobble.png^[colorize:#c8c8c1:120"},
 	is_ground_content = true,
@@ -150,7 +150,7 @@ minetest.register_node ("mycobble:silver_cobble",{
 	}),
 })
 
-minetest.register_node ("mycobble:sandstone_cobble",{
+core.register_node ("mycobble:sandstone_cobble",{
 	description = "Sandstone Cobble",
 	tiles = {"default_cobble.png^[colorize:#e1da94:120"},
 	is_ground_content = true,
@@ -161,7 +161,7 @@ minetest.register_node ("mycobble:sandstone_cobble",{
 	}),
 })
 
-minetest.register_node ("mycobble:desert_sandstone_cobble",{
+core.register_node ("mycobble:desert_sandstone_cobble",{
 	description = "Desert Sandstone Cobble",
 	tiles = {"default_cobble.png^[colorize:#f4b05c:120"},
 	is_ground_content = true,
@@ -172,7 +172,7 @@ minetest.register_node ("mycobble:desert_sandstone_cobble",{
 	}),
 })
 
-minetest.register_node ("mycobble:desert_sandstone_gravel",{
+core.register_node ("mycobble:desert_sandstone_gravel",{
 	description = "Desert Sandstone Gravel",
 	tiles = {"default_gravel.png^[colorize:#f4b05c:120"},
 	is_ground_content = true,
@@ -183,7 +183,7 @@ minetest.register_node ("mycobble:desert_sandstone_gravel",{
 	}),
 })
 
-minetest.register_node ("mycobble:desert_sandstone_sand",{
+core.register_node ("mycobble:desert_sandstone_sand",{
 	description = "Desert Sandstone Sand",
 	tiles = {"default_sand.png^[colorize:#ef8a3d:120"},
 	is_ground_content = true,
